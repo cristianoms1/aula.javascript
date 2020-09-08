0d56465cd1648ac13b85d0b47591af540b739ac7
cristiano.meneses <cristiano@avancoinfo.com.br>
2020-09-08 13:39:08 -0300

    Aula 01 - sobre strings

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Cristiano Soares";
+var n1 = 29;
+var n2 = 10;
+var frase = "Cruzeiro é o melhor time do mundo";
+// alert(nome + " tem " + idade + " anos");
+// alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toUpperCase());
+// alert(frase.replace("Cruzeiro", "CRB"));
